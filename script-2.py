23de5fd3db6de1893870861c578aae3989bc6def
peter chang <pech6593@colorado.edu>
2023-02-06 15:07:15 -0700

    update

--- script-2.py
+++ script-2.py
@@ -7,6 +7,7 @@ import clipboard_and_style_sheet as cr
 import td_phase_correct as td
 from tqdm import tqdm
 import os
+import scipy.constants as sc
 
 cr.style_sheet()
 
@@ -38,9 +39,19 @@ freq_a = np.fft.rfftfreq(ppifg // 100, d=1 / 100e6) * ppifg * 1e-12
 avg = np.load("data/avg.npy")
 ft_f = np.fft.rfft(avg)
 ft_a = np.fft.rfft(avg[center - ppifg // 100 // 2: center + ppifg // 100 // 2])
+wl_f = sc.c * 1e9 / (freq_f * 1e12)
+wl_a = sc.c * 1e9 / (freq_a * 1e12)
 
 # ______________ plotting _____________________________________________________
-plt.plot(freq_f, abs(ft_f) / abs(ft_a)[100:].max())
-plt.plot(freq_a, abs(ft_a) / abs(ft_a)[100:].max())
+plt.plot(freq_f, abs(ft_f) / abs(ft_a)[100:].max(), label="100 MHz resolution")
+plt.plot(freq_a, abs(ft_a) / abs(ft_a)[100:].max(), label="10 GHz resolution")
 plt.ylim(-.02, 1.1)
 plt.xlim(756, 800)
+plt.legend(loc='best')
+
+plt.figure()
+plt.plot(wl_f, abs(ft_f) / abs(ft_a)[100:].max(), label="100 MHz resolution")
+plt.plot(wl_a, abs(ft_a) / abs(ft_a)[100:].max(), label="10 GHz resolution")
+plt.ylim(-.02, 1.1)
+plt.xlim(sc.c * 1e9 / 800e12, sc.c * 1e9 / 756e12)
+plt.legend(loc='best')
